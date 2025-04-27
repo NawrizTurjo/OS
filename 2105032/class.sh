eb47335d2fe76918a3cb04dabbedd631bb3b0166
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2025-04-27 22:24:49 +0600

    initialized

---
+++ 2105032/class.sh
@@ -0,0 +1,85 @@
+
+
+# printf("Heloowww :>");
+
+# echo "Hellowww :>"
+
+# x="meowww <3"
+# echo "$x"
+
+# echo "Enter Your Name:"
+
+# read name
+
+# echo "Hello $name"
+
+# ls
+
+
+# Conditional
+
+# echo -n "Enter a number: "
+# read x
+
+# # echo "You entered $x"
+
+# if [[ $x -eq 10 ]]; then # must include the two spaces as it is here
+#     echo "yeeeeaaaa....you're lucky"
+# fi
+
+
+# if (( x==10 )); then
+#     echo "yooooo"
+# fi
+
+# for loop
+
+# for ((i = 0;i < 100; i++));do ####### C Style syntax
+#     echo "I am printing with for loop $i"
+# done
+
+# mkdir image
+# for i in $(ls source)
+# do
+#     filename=$i
+#     id=${i::-4}
+#     # echo "$id"
+#     # echo "$filename"
+#     mkdir "image/$id"
+#     mv "source/$filename" "image/$id"
+# done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2105032/hi.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hellooooowww :>\n");
+
+    return 0;
+}
